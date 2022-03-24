bbc3424885f7635493e48424223e597ffef57cb3
Ethan <eth22mer@gmail.com>
2022-03-24 19:54:33 -0400

    Working expanding photos Scroller

--- Frontend/portfolio-2022/src/components/Projectcard.js
+++ Frontend/portfolio-2022/src/components/Projectcard.js
@@ -12,38 +12,38 @@ const Component = (props) => {
     const [showPhotos, setShowPhotos] = useState(false)
 
     // JSX return
-    return(
+    return (
         <>
-        <div className="project-card-wrapper">
-            
-                <div className="project-card-border" onMouseDown={() => setShowPhotos(!showPhotos)}>
-                    <div className={showPhotos ? 'images-carousel-stacked' : 'images-carousel-unstacked'}>
+            <div className="project-card-wrapper">
 
-                        {Images &&
-                            Images.map((e, i) => {
-                                for (var key in e) {
-                                    console.log(i, key, e[key])
-                                    return (
-                                        <div className="card-image">
-                                            <img classname={"image-" + i} key={i} src={e[key]} alt={key}></img>
-                                        </div>
-                                    )
-                                }
+                <div className=" project-card-border" onMouseDown={() => setShowPhotos(!showPhotos)}>
+                    <div className={showPhotos ? "carousel-container stacked" : "carousel-container unstacked"}>
+                        <div className="imc">
+                            {Images &&
+                                Images.map((e, i) => {
+                                    for (var key in e) {
+                                        console.log(i, key, e[key])
+                                        return (
+                                            <div className={"card-image image-" + i}>
+                                                <img className={"image-"} key={i} src={e[key]} alt={key}></img>
+                                            </div>
+                                        )
+                                    }
 
-                            })
-                        }
+                                })
+                            }
+                        </div>
                     </div>
-                <div className="card-image-placeholder"></div>
-                <div className="card-text-container">
-                    <h3 className="card-title">{Title}</h3>
+                    <div className="card-text-container">
+                        <h3 className="card-title">{Title}</h3>
 
-                    <div className="card-description">
-                        <p>
-                            {Blurb}
-                        </p>
-                </div>
-               
-                </div>
+                        <div className="card-description">
+                            <p>
+                                {Blurb}
+                            </p>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </>
