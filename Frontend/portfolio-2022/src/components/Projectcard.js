3725c555ade270bd143fcca61ba9e5ee50749586
Ethan <eth22mer@gmail.com>
2022-03-24 17:37:36 -0400

    image stacking and unstacking

--- Frontend/portfolio-2022/src/components/Projectcard.js
+++ Frontend/portfolio-2022/src/components/Projectcard.js
@@ -8,26 +8,32 @@ const Component = (props) => {
     const Title = props?.props?.Title
     const Images = props?.props?.Images
     // State Vasriables
+
+    const [showPhotos, setShowPhotos] = useState(false)
+
     // JSX return
     return(
         <>
-            <div className="project-card-wrapper">
-                <div>
-                    {Images &&
-                        Images.map((e, i) => {
-                            for (var key in e) {
-                                console.log(key, e[key])
-                                return (
-                                    
-                                        <div className="card-image container">
-                                            <img key={i} src={e[key]} alt={key}></img>
+        <div className="project-card-wrapper">
+            
+                <div className="project-card-border" onMouseDown={() => setShowPhotos(!showPhotos)}>
+                    <div className={showPhotos ? 'images-carousel-stacked' : 'images-carousel-unstacked'}>
+
+                        {Images &&
+                            Images.map((e, i) => {
+                                for (var key in e) {
+                                    console.log(i, key, e[key])
+                                    return (
+                                        <div className="card-image">
+                                            <img classname={"image-" + i} key={i} src={e[key]} alt={key}></img>
                                         </div>
                                     )
-                            }
+                                }
 
-                        })
-                    }
-                </div>
+                            })
+                        }
+                    </div>
+                <div className="card-image-placeholder"></div>
                 <div className="card-text-container">
                     <h3 className="card-title">{Title}</h3>
 
@@ -37,6 +43,7 @@ const Component = (props) => {
                         </p>
                 </div>
                
+                </div>
                 </div>
             </div>
         </>
