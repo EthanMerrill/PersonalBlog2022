1fbfb03e34b7eb6ef49e139ef88c5312c9c45ace
Ethan <eth22mer@gmail.com>
2023-03-07 22:23:25 -0800

    fixed card mobile view

--- Frontend/portfolio-2022/src/components/ProjectCard2/ProjectCard2.tsx
+++ Frontend/portfolio-2022/src/components/ProjectCard2/ProjectCard2.tsx
@@ -14,15 +14,15 @@ const ProjectCard2 = (props:projectCardProps) => {
     return (
         <>
         <a href={extLink ? extLink : ''} className='w-3/5 mr-36 sm:mr-auto my-6 mx-auto'>
-        <div className="rounded-sm  bg-gray-100 drop-shadow-lg m-0 hover:shadow-lg animate-fade flex flex-row ">
+        <div className="rounded-sm  bg-gray-100 drop-shadow-lg m-0 hover:shadow-lg animate-fade flex flex-row flex-wrap ">
             {(imageLink && imageAlt) &&
-                    <img src={imageLink} alt={imageAlt} className=" object-cover w-52 rounded-l-sm" />
+                    <img src={imageLink} alt={imageAlt} className=" object-cover w-52 md:w-full md:h-32 md:rounded-t-sm rounded-l-sm" />
                 }
             <div className="flex flex-row p-5 justify-start sm:flex-wrap sm:justify-center sm:pb-5 gap-5">
                 <div className="flex flex-col text-sm flex-shrink">
-                    <div className="flex flex-row text-lg gap-1 sm:text-sm sm:mx-auto">
+                    <div className="flex flex-row flex-wrap text-lg gap-1 sm:text-sm sm:mx-auto md:pb-4">
                         <h4 className="opacity-90 sm:text-sm">{title}</h4>
-                        <h4 className="text-blue-accent sm:text-sm">|</h4>
+                        <h4 className="text-blue-accent md:hidden">|</h4>
                         <p className="opacity-50 sm:text-sm">{subTitle}</p>
                     </div>
                     <div className={clsx("text-sm opacity-50 mt-2 text-left", extLink && "pb-8")}>{blurb}</div>
