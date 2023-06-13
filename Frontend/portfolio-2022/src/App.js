a550015d95eee164087a79d80a6025d1fa9be6bd
Ethan <eth22mer@gmail.com>
2023-06-13 10:01:51 -0400

    more mobile optimizations

--- Frontend/portfolio-2022/src/App.js
+++ Frontend/portfolio-2022/src/App.js
@@ -24,7 +24,7 @@ export default function App() {
 
 
   return (
-    <body>
+    <div className='main-body'>
       <div className='easter-egg absolute top-[-10px]'><p className=' z-10 relative text-white'>Hello again!</p></div>
       <div className="app z-10 relative">
         <AppHeader />
@@ -48,7 +48,7 @@ export default function App() {
                 // style={{ fontSize: '2em', display: 'inline-block' }}
                 /> :
                 <TypeAnimation
-                  className="m-auto h-full px-10 leading-8 font-medium text-main-text-gray max-w-[800px]"
+                  className="m-auto h-[200px] px-10 leading-8 font-medium text-main-text-gray max-w-[800px]"
                   sequence={[
                     `Hi, I'm Ethan Merrill. I'm a frontend software engineer. I hold a Bachelor of Science in Engineering Management from Worcester Polytechnic Institute.`, // Deletes 'One' and types 'Two'
                     1000, // Waits 2s
@@ -91,12 +91,12 @@ export default function App() {
             <Categorysection sectionNumber={4} title={'Contact'}>
               <div className='h-[70vh] '>
                 <div className='h-[70vh] contain-paint bg-navy w-full'>
-                  <div className='relative  bg-white right-[300px] top-[-50vh] h-[60vh] w-[200vw] rotate-[-15deg] drop-shadow-md'>
+                  <div className='relative  bg-white right-[300px] top-[-50vh] h-[70vh] w-[200vw] rotate-[-15deg] drop-shadow-md'>
                   </div>
 
                 </div>
                 <div className='sticky bottom-36 w-full '>
-                  <div className=' text-white mx-auto w-1/5 text-center'>
+                  <div className=' text-white mx-auto w-4/5 text-center'>
                     <h1 className='text-xl'>
                       Contact
                     </h1>
@@ -109,6 +109,6 @@ export default function App() {
           </div>
         </div>
       </div >
-    </body>
+    </div>
   );
 }
