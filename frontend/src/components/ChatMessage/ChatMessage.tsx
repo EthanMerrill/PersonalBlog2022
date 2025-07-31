19667e842644b09ff7bb1c1268c18d5fa61d15f7
Ethan <eth22mer@gmail.com>
2025-07-31 16:07:32 -0400

    improve message formatting, particularly on mobile

--- frontend/src/components/ChatMessage/ChatMessage.tsx
+++ frontend/src/components/ChatMessage/ChatMessage.tsx
@@ -10,17 +10,30 @@ export interface ChatMessageProps {
     completed?: CallbackFunction;
 }
 
-const ChatMessage = (props: ChatMessageProps) => (
+interface AuthorAndDateProps {
+    author?: string;
+    timestamp?: number;
+}
 
+const AuthorAndDate = ({ author, timestamp }: AuthorAndDateProps) => (
+    <div className="flex items-center mt-1 pl-1">
+        <div className="chat-message-author text-xxs text-slate opacity-50 mr-2">{author}</div>
+        <div className="chat-message-timestamp text-xxs text-slate opacity-30">
+            {timestamp ? new Date(timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : ''}
+        </div>
+    </div>
+);
+
+const ChatMessage = (props: ChatMessageProps) => (
     !props.user ? (
-        <div className='mb-2 mr-40'>
-            <div className="chat-message bg-blue-accent p-3 rounded-md mr-auto">
+        <div className="mb-2 mr-40 sm:mr-0 sm:w-full">
+            <div className="chat-message bg-blue-accent p-3 rounded-md mr-auto sm:mr-0 sm:w-full">
                 <div className="chat-message-text text-white font-thin text-sm">
                     <TypeAnimation
                         sequence={[
                             props.message,
                             () => {
-                                if (props.completed) props.completed(true); // Call completed callback if it exists
+                                if (props.completed) props.completed(true);
                             }
                         ]}
                         speed={70}
@@ -30,24 +43,18 @@ const ChatMessage = (props: ChatMessageProps) => (
                     />
                 </div>
             </div>
-            <div className="flex items-center mt-1 pl-1">
-                <div className="chat-message-author text-xxs text-slate opacity-50 mr-2">{props.author}</div>
-                <div className="chat-message-timestamp text-xxs text-slate opacity-30">{props.timestamp ? new Date(props.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : ''}</div>
-            </div>
+            <AuthorAndDate author={props.author} timestamp={props.timestamp} />
         </div>
     ) : (
-        <div className='mb-2 ml-40 flex flex-col items-end'>
-            <div className="chat-message bg-gray-300 p-3 rounded-md">
+        <div className="mb-2 ml-40 flex flex-col items-end sm:ml-0 sm:w-full">
+            <div className="chat-message bg-gray-300 p-3 rounded-md sm:w-full">
                 <div className="chat-message-text font-thin text-right text-sm">
                     <p>
                         {props.message}
                     </p>
                 </div>
             </div>
-            <div className="flex items-center mt-1 pr-1">
-                <div className="chat-message-timestamp text-xs text-slate opacity-60 mr-2">{props.timestamp ? new Date(props.timestamp).toLocaleTimeString() : ''}</div>
-                <div className="chat-message-author text-xs text-slate opacity-80">{props.author}</div>
-            </div>
+            <AuthorAndDate author={props.author} timestamp={props.timestamp} />
         </div>
     )
 );
